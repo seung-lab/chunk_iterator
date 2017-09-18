b0664b4c1da9ff96e451d3eb372cad58e7604bfd
Ran Lu <ranl@princeton.edu>
2017-09-18 17:07:24 -0400

    Update the test script

--- tests/test_chunk_iterator.py
+++ tests/test_chunk_iterator.py
@@ -1,12 +1,13 @@
 from chunk_iterator import ChunkIterator
 
-v = ChunkIterator([10240, 7680, 0, 65015, 43715, 1002], [1024, 1024, 1024])
+#v = ChunkIterator([10240, 7680, 0, 65015, 43715, 1002], [1024, 1024, 1024])
+v = ChunkIterator([0,0,0,2048,2048,256], [500, 500, 100])
 print v.offset()
-print v.dataDim()
-print v.dataBBox()
-print v.topMipLevel()
-print v.mipLevelDataDimInChunks(4)
-print v.mipLevelDataDimInChunks(5)
+print v.data_dim()
+print v.data_bbox()
+print v.top_mip_level()
+print v.mip_level_data_dim_in_chunks(4)
+print v.mip_level_data_dim_in_chunks(5)
 
 for c in v:
     c.print_chunk_info()
