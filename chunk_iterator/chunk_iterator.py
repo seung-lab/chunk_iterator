c166038260da277912996b47bd652bda07b13044
Ran Lu <ranl@princeton.edu>
2019-06-12 14:53:15 -0400

    Starting iterating from a specific chunk
    This is a temporary hack for minnie inferences, will make a more generic
    interface allow to start iteration from a specify chunk index/offset

--- chunk_iterator/chunk_iterator.py
+++ chunk_iterator/chunk_iterator.py
@@ -3,13 +3,17 @@ import math
 from .chunk import Chunk
 
 class ChunkIterator:
-    def __init__(self, volume_bbox, chunk_dim):
+    def __init__(self, volume_bbox, chunk_dim, start_from = None):
         self._offset = volume_bbox[0:3]
         self._chunk_dim = chunk_dim
         self._data_dim = [volume_bbox[i+3]-volume_bbox[i] for i in range(3)]
         dim_in_chunks = self.mip_level_data_dim_in_chunks(0)
         self._top_mip_level = int(math.ceil(math.log(max(dim_in_chunks))/math.log(2)))
         self._chunks = [Chunk(self, self._top_mip_level, [0,0,0])]
+        self._target = [self._top_mip_level, 0, 0, 0]
+        self._started = False
+        if start_from:
+            self._target = start_from
 
     def offset(self):
         return self._offset
@@ -39,6 +43,25 @@ class ChunkIterator:
     def __next__(self):
         if len(self._chunks) == 0:
             raise StopIteration
+        while not self._started:
+            if len(self._chunks) == 0:
+                raise StopIteration
+            c = self._chunks[0]
+            if c.mip_level() != self._target[0]:
+                self._chunks += c.children()
+                self._chunks.pop(0)
+                continue
+
+            for cc in self._chunks:
+                target = self._target[1:]
+                if cc.coordinate() == target:
+                    c = cc
+                    self._started = True
+                    break
+            if self._started:
+                self._chunks = c.children()
+                return c
+
         c = self._chunks.pop(0)
         self._chunks += c.children()
         return c
