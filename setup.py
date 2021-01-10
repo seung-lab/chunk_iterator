520e7f017c2d5e91f100d2f09b7131537b043a2c
Ran Lu <ranl@princeton.edu>
2021-01-10 15:44:15 -0500

    setup.py placeholder

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="chunk-iterator", # Replace with your own username
+    version="0.0.1",
+    author="Ran Lu",
+    author_email="ranl@princeton.edu",
+    description="Recursively dividing input bbox into chunks as an octree",
+    url="https://github.com/seung-lab/chunk_iterator",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
